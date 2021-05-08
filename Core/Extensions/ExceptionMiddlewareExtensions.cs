1e1a0082774ab405bd038c384dd225eeced98d03
Atakan Reyhanioglu <atakan.reyhanioglu@gmail.com>
2021-05-08 13:22:35 +0300

    extensionsAdded

---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+
