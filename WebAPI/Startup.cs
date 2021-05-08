3040985f90c72f2f568bb6ffc5c83f6c1fe6f75c
Atakan Reyhanioglu <81529567+atakanreyhanioglu@users.noreply.github.com>
2021-05-08 13:23:57 +0300

    Update Startup.cs

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -82,6 +82,8 @@ namespace WebAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+                        app.ConfigureCustomExceptionMiddleware();
+
         app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
